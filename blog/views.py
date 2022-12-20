9e8326e252ad66fe0922ca2eac5f4fe5cacb139d
rose000227 <rose000227@naver.com>
2022-12-20 17:38:16 +0900

    add static_root

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -38,3 +38,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    post_remove.info(request, "글이 삭제되었습니다.")
+    return redirect('blog:post_detail', pk=post.pk)
